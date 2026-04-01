d2369b3d83d0faa811c85a6f2e86ee4473de4a8a
Abdul Aziz <connect2abdulaziz@gmail.com>
2026-04-01 17:24:08 +0500

    Add Favicons and web manifest

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,31 +18,10 @@ export const metadata = {
   description:
     "Aluminox Technical Services LLC — Expert Facade Solutions in UAE. Over 20 years of expertise.",
   icons: {
-    icon: [
-      {
-        url: "/favicon-48.png",
-        sizes: "48x48",
-        type: "image/png",
-      },
-      {
-        url: "/favicon-192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        url: "/favicon.ico",
-        sizes: "any",
-        type: "image/x-icon",
-      },
-    ],
-    apple: [
-      {
-        url: "/apple-touch-icon.png",
-        sizes: "180x180",
-        type: "image/png",
-      },
-    ],
+    icon: "/favicon-192.png",
+    apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({ children }) {
